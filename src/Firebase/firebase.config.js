a09b9ab4af63d48f1eca7a6e0a09e9457b732df6
fname lname <monzurulislam997@gmail.com>
2021-10-15 10:27:42 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBh8QlzCQud8lrTsfj0yp3Njji2I-UioZo",
+    authDomain: "simple-emajhon-firebase.firebaseapp.com",
+    projectId: "simple-emajhon-firebase",
+    storageBucket: "simple-emajhon-firebase.appspot.com",
+    messagingSenderId: "530455717933",
+    appId: "1:530455717933:web:998b86b92587488da230a4"
+  };
+  export default firebaseConfig;
